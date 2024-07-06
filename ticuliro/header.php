0fc8023d9e2aacaa02a6e7a557f986bc0152a95f
Murilo Torres <133931037+devMuriloTS@users.noreply.github.com>
2024-07-05 22:07:50 -0300

    att

---
+++ ticuliro/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="headerstyle.css">
+    <title>King Can't Scape!</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <a href="index.php">
+                <div class="imagemLogo">
+                    <img src="reizinho.png" alt="Logo do jogo. Rei." width="50px" height="50px">
+                </div>
+                <div class="nomeLogo">
+                    <span class="Nome">King Can't Scape!</span>
+                </div>
+            </a>
+        </div>
+
+        <div class="btnHeader">
+            <a href="baixar.php" class="btn-download">Download</a>
+        </div>
+
+        <div class="login">
+            <a href="login.php">
+                <span>
+                    Login
+                    <div class="iconLogin">
+                        <!-- Se desejar, adicione um ícone de login aqui -->
+                    </div>
+                </span>
+            </a>
+        </div>
+    </header>
+</body>
+</html>
---
+++ ticuliro/index.php
@@ -0,0 +1,38 @@
+<?php include_once "header.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>King Can't Scape!</title>
+</head>
+<body>
+    <main class="container">
+        <section class="hero">
+            <div class="hero-content">
+                <div class="hero-text">
+                    <p class="hero-text-left">Faça parte</p>
+                    <img src="reizinho.png" alt="Logo do reizinho" class="site-logo">
+                    <p class="hero-text-right">da aventura</p>
+                </div>
+                <a href="#about-game" class="btn-scroll">Saiba mais</a>
+            </div>
+        </section>
+    </main>
+    <section id="about-game" class="about-game">
+        <div class="about-game-content">
+            <div class="about-game-text">
+                <h2>Sobre o Jogo</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget lorem in urna mattis aliquet.</p>
+                <a href="baixar.php" class="btn-download">Download</a>
+            </div>
+            <div class="game-image-container">
+                <img src="fundo.jpeg" alt="Imagem do jogo" class="game-image">
+                <div class="diagonal-line"></div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
